39b1d40f6f4d707239fc7c245da2277245f553bc
Rahul Panchal <rahulpydev22@gmail.com>
2023-06-17 22:46:10 +0530

    init

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDwgNC2XQw3qXelMa07MRoHI_yh6NZSuB4",
+    authDomain: "clips-3a276.firebaseapp.com",
+    projectId: "clips-3a276",
+    storageBucket: "clips-3a276.appspot.com",
+    messagingSenderId: "647733807769",
+    appId: "1:647733807769:web:2517c39af319e54302c608",
+  },
+};
